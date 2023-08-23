b640e1de7d68321db7e5b4623bb5678c60f4428d
Juno Park <junopark919@gmail.com>
2023-08-23 13:22:05 -0700

    Modify the way to open accordion items

--- accordion/src/components/Accordion.js
+++ accordion/src/components/Accordion.js
@@ -1,16 +1,37 @@
+import { useState } from 'react';
 import AccordionItem from './AccordionItem';
 
 export default function Accordion({ faqs }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {faqs.map((item, i) => (
         <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
           num={i}
           title={item.title}
-          text={item.text}
           key={item.title}
-        />
+        >
+          {item.text}
+        </AccordionItem>
       ))}
+
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        num={22}
+        title="Test 1"
+        key="test 1"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
