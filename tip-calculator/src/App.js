7cc73438d843509ca889e86183afb13233689ab9
Juno Park <junopark919@gmail.com>
2023-08-25 13:51:51 -0700

    Modify the wrong part I built myself according to class guide

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -1,29 +1,21 @@
 import { useState } from 'react';
 
 export default function App() {
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
+}
+
+// I should've first divided the app into multiple components
+function TipCalculator() {
   const [bill, setBill] = useState('');
   const [tipForMe, setTipForMe] = useState(0);
   const [tipForFriend, setTipForFriend] = useState(0);
 
-  function handleBill(e) {
-    setBill(Number(e.target.value));
-  }
-
-  function handleTipForMe(e) {
-    if (!bill) {
-      return;
-    } else {
-      setTipForMe(Number(e.target.value) * bill);
-    }
-  }
-
-  function handleTipForFriend(e) {
-    if (!bill) {
-      return;
-    } else {
-      setTipForFriend(Number(e.target.value) * bill);
-    }
-  }
+  // derived state
+  const tip = bill * ((tipForMe + tipForFriend) / 2 / 100);
 
   function handleReset() {
     setBill('');
@@ -33,39 +25,63 @@ export default function App() {
 
   return (
     <div>
-      <div>
-        How much was the bill?
-        <input
-          type="text"
-          placeholder="Bill value"
-          value={bill}
-          onChange={(e) => handleBill(e)}
-        />
-      </div>
-      <Tip handleTip={handleTipForMe}>How do you like the service?</Tip>
-      <Tip handleTip={handleTipForFriend}>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <Tip tip={tipForMe} onSetTip={setTipForMe}>
+        How do you like the service?
+      </Tip>
+      <Tip tip={tipForFriend} onSetTip={setTipForFriend}>
         How did your friend like the service?
       </Tip>
       {bill > 0 && (
-        <h3>{`You pay $${bill + tipForMe + tipForFriend} ($${bill} + $${
-          tipForMe + tipForFriend
-        }) tip`}</h3>
+        <>
+          {' '}
+          <Output bill={bill} tip={tip} />
+          <Reset onReset={handleReset} />
+        </>
       )}
-      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
 
-function Tip({ bill, handleTip, children }) {
+function BillInput({ bill, onSetBill }) {
   return (
     <div>
-      {children}
-      <select onChange={(e) => handleTip(e)}>
-        <option value={0}>Dissatisfied (0%)</option>
-        <option value={0.05 / 2}>It was okay (5%)</option>
-        <option value={0.1 / 2}>It was good (10%)</option>
-        <option value={0.2 / 2}>Absolutely amazing! (20%)</option>
+      <label>How much was the bill?</label>
+      <input
+        type="text"
+        placeholder="Bill value"
+        value={bill}
+        onChange={(e) => onSetBill(Number(e.target.value))}
+      />
+    </div>
+  );
+}
+
+function Tip({ bill, tip, onSetTip, children }) {
+  console.log(tip);
+
+  // I thought I assign decimal into value at first, but integer is better here for output
+  return (
+    <div>
+      <label>{children}</label>
+      <select value={tip} onChange={(e) => onSetTip(Number(e.target.value))}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
       </select>
     </div>
   );
+}
+
+function Output({ bill, tip }) {
+  return (
+    <h3>
+      You pay ${bill + tip} (${bill} + ${tip} tip)
+    </h3>
+  );
+}
+
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
 }
