e23b0f67784715aec815b0bc069dc85eea19f126
Juno Park <junopark919@gmail.com>
2023-10-11 17:27:26 -0500

    Add a loading indicator

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -56,16 +56,19 @@ const KEY = '8c2ee7d3';
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = 'interstellar';
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
       console.log(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -90,9 +93,7 @@ export default function App() {
           }
         /> */}
 
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
@@ -101,6 +102,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 // Structural
